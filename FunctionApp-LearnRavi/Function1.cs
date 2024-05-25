65c6bdec80d1d25b366494e63eeb0159fb3e271d
bhoskar-ravi <ravibhoskar@outlook.com>
2024-05-25 22:34:44 +0530

    second commit

--- FunctionApp-LearnRavi/Function1.cs
+++ FunctionApp-LearnRavi/Function1.cs
@@ -27,7 +27,7 @@ namespace FunctionApp_LearnRavi
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully. This is deployed by CI/CD.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully. This is deployed by CI/CD. Finally";
 
             return new OkObjectResult(responseMessage);
         }
